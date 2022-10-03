2e1f6f459806d8954cb7e5576aa43b6b7bed23bf
kmindc <keefemitchell@gmail.com>
2022-10-03 15:53:07 +1000

    Starting speech to text function.

--- DECO3801_project/frontend/src/components/Home.js
+++ DECO3801_project/frontend/src/components/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ContentHolder from './Content';
 import CaptureVideo from './CaptureVideo';
+import VoiceRecog from './VoiceRecog';
 
 function Home(props) {
   return (
     <ContentHolder>
       <h1> This is our homepage</h1>
       <CaptureVideo></CaptureVideo>
+      <VoiceRecog></VoiceRecog>
     </ContentHolder>
   );
 }
