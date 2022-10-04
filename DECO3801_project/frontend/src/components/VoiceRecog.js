7c7d519a3a2885a39481779356ec19508d3f2db9
kmindc <keefemitchell@gmail.com>
2022-10-04 13:38:25 +1000

    Speech recognition function updated to recognise note taking commands

--- DECO3801_project/frontend/src/components/VoiceRecog.js
+++ DECO3801_project/frontend/src/components/VoiceRecog.js
@@ -3,21 +3,38 @@ import * as Annyang from 'annyang';
 
 export default function VoiceRecog() {
     if (annyang) {
-       // Let's define a command.
-       var anything = function(anything) {
+        var hello = function(hello) {
+            alert('Hi! I can hear you.');
+        };
+
+        var createNote = function(createNote) {
+            alert('Sampling or Media Production?');
+        };
+
+        var sampling = function(createNote) {
+            alert('sampling note');
+        };
+
+        var mediaProduction = function(createNote) {
+            alert('media production note');
+        };
+
+        var anything = function(anything) {
             alert(anything);
         };
+
         var commands = {
-            '*anything': anything
+            'hello': hello,
+            'create note': createNote,
+            'sampling': sampling,
+            'media production': mediaProduction,
+            'write note *anything': anything
         };
-       // This should be true
-       console.log(annyang.isListening())
-       // Add our commands to annyang
-       annyang.addCommands(commands);
-       // Start listening.
-       annyang.start();
-       annyang.addCallback('soundstart', function() {
-         console.log('sound detected');
-       });
-     }
+        // This should be true
+        console.log(annyang.isListening())
+        // Add our commands to annyang
+        annyang.addCommands(commands);
+        // Start listening.
+        annyang.start();
+    }
 }
