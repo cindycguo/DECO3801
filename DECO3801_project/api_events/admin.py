5c932f743b15a960e44b515b223b3e5ab09ec13e
cindycguo <cindycguo@icloud.com>
2022-10-10 21:33:01 +1000

    Modified models.py to include one Session class and fixed the related fields.

--- DECO3801_project/api_events/admin.py
+++ DECO3801_project/api_events/admin.py
@@ -3,6 +3,3 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Session)
-admin.site.register(SessionReason)
-admin.site.register(SessionStatus)
-admin.site.register(SessionType)
