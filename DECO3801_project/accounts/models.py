1be475bfe3f323b071f44741184346134fa378ad
cindycguo <cindycguo@icloud.com>
2022-10-13 23:23:01 +1000

    Added Employee and Supervisor in accounts folder. Employee model is linked to User model

--- DECO3801_project/accounts/models.py
+++ DECO3801_project/accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Supervisor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    sup_no = models.IntegerField(_('employee number'), primary_key=True)
+    first_name = models.CharField(_('First name'), max_length=15)
+    last_name = models.CharField(_('Last name'), max_length=15)
+    hire_date = models.DateField(_('hire date'))
+
+    def __str__(self, user):
+        return user.first_name
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    emp_no = models.IntegerField(_('employee number'), primary_key=True)
+    hire_date = models.DateField(_('hire date'))
+    supervisor = models.ForeignKey(Supervisor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name + " " + str(self.emp_no)
+
--- DECO3801_project/accounts/views.py
+++ DECO3801_project/accounts/views.py
@@ -1,9 +1,21 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render, redirect
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    def register(self, request):
+        if request.method == "GET":
+            return render(request, template_name, {"form": form_class})
+
+        elif request.method == "POST":
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                login(request, user)
+                return redirect(success_url)
